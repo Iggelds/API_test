410f60077e9ae30d78bc766140f42efbff1ce69d
Aleksandr Guido <iggelds@gmail,com>
2022-09-14 20:21:23 +0200

    createPetTest code improvements

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -13,22 +13,25 @@ import static org.apache.http.HttpStatus.SC_OK;
 
 public class AppTest {
 
+    private final String baseUrl = "https://petstore.swagger.io/v2/";
+
     @Test
     public void createPetTest() {
-        RequestSpecification requestSpecification = RestAssured.given();
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Accept", "application/json");
-        headers.put("Content-Type", "application/json");
-        requestSpecification.headers(headers);
+        Pet exptectedPet = new Pet("Toby");
 
-        Pet exptectedPet = new Pet();
-        exptectedPet.setName("Toby");
-
-        Response response = requestSpecification.body(exptectedPet).expect().statusCode(SC_OK)
-                .when().post("https://petstore.swagger.io/v2/pet");
+        Response response = request().body(exptectedPet).expect().statusCode(SC_OK)
+                .when().post(baseUrl + "pet");
 
         Pet pet = response.jsonPath().getObject("", Pet.class);
         Assert.assertTrue(pet.getId() > 0, "Expected id exists");
         Assert.assertEquals(pet.getName(), "Toby", "Expected valid name");
+    }
+
+    private static RequestSpecification request() {
+        RequestSpecification requestSpecification = RestAssured.given();
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Accept", "application/json");
+        headers.put("Content-Type", "application/json");
+        return requestSpecification.headers(headers);
     }
 }
