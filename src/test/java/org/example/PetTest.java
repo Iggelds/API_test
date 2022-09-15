8ca826f0e05bb86b194e2a3b6f8bbf3a2bf58d10
Aleksandr Guido <iggelds@gmail,com>
2022-09-15 12:11:02 +0200

    added deletePetTest

--- src/test/java/org/example/PetTest.java
+++ src/test/java/org/example/PetTest.java
@@ -9,9 +9,10 @@ import org.testng.annotations.Test;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.apache.http.HttpStatus.SC_NOT_FOUND;
 import static org.apache.http.HttpStatus.SC_OK;
 
-public class AppTest {
+public class PetTest {
 
     private final String baseUrl = "https://petstore.swagger.io/v2/";
 
@@ -27,11 +28,25 @@ public class AppTest {
         Assert.assertEquals(pet.getName(), "Toby", "Expected valid name");
     }
 
+    @Test
+    public void deletePetTest() {
+        Pet exptectedPet = new Pet("Toby");
+
+        Response response = request().body(exptectedPet).expect().statusCode(SC_OK)
+                .when().post(baseUrl + "pet");
+
+        Pet pet = response.jsonPath().getObject("", Pet.class);
+        String petUrl = baseUrl + "pet/" + pet.getId();
+        request().when().delete(petUrl).then().statusCode(SC_OK);
+        request().when().get(petUrl).then().statusCode(SC_NOT_FOUND);
+    }
+
     private static RequestSpecification request() {
         RequestSpecification requestSpecification = RestAssured.given();
         Map<String, String> headers = new HashMap<>();
         headers.put("Accept", "application/json");
         headers.put("Content-Type", "application/json");
+        headers.put("api-key", "application/json");
         return requestSpecification.headers(headers);
     }
 }
